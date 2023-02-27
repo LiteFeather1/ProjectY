41d33e3fd4734e334b19b3f062f926ebb76d8b3b
LiteFeather1 <kevin_gestel@eticalgarve.com>
2023-02-27 15:12:24 +0000

    Updated Shooter Layout Added a postion nudger to shooter targets

--- ProjectY/Assets/_Scripts/Shooter/RandomTarget.cs
+++ ProjectY/Assets/_Scripts/Shooter/RandomTarget.cs
@@ -10,8 +10,9 @@ namespace Shooter
         private void Awake()
         {
             int r = Random.Range(0, _possibleTargets.Length);
-            var target = Instantiate(_possibleTargets[r], transform.position, Quaternion.identity);
+            var target = Instantiate(_possibleTargets[r], transform.position, Quaternion.identity, transform.parent);
             target.TargetScore.SetLaneMultiplier(_laneMultipler);
+            gameObject.SetActive(false);
         }
     }
 }
